44d04b647e383bacfb91a7b6185f6b25ff7cf63b
jose antonio <jsancesc@gmail.com>
2023-10-15 13:45:24 +0200

    s

--- ejercicio.py
+++ ejercicio.py
@@ -17,20 +17,20 @@ def solve(tablero, disco, origen, destino, auxiliar, movimientos):
         destino.apilar(origen.desapilar())
         solve(tablero, disco - 1, auxiliar, destino, origen, movimientos)
 
-# Inicializar el tablero con 3 discos
+
 torre1, torre2, torre3 = getTablero(3)
 tablero = [torre1, torre2, torre3]
 
-# Resolver el problema y obtener la lista de movimientos
+
 movimientos = []
 solve(tablero, 3, torre1, torre3, torre2, movimientos)
 
-# Mostrar la lista de movimientos
+
 print("Lista de movimientos:")
 for movimiento in movimientos:
     print(movimiento)
 
-# Mostrar contenido final de las pilas
+
 print("\nContenido final de las pilas:")
 for i, torre in enumerate(tablero):
     print(f'Torre {i + 1}: {torre}')
